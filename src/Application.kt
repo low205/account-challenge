3e69cb508cea167f181436a852098e04cf7f0767
Evgeniy Maltsev <low205@gmail.com>
2019-10-14 12:30:24 +0200

    Initial system setup

---
+++ src/Application.kt
@@ -0,0 +1,31 @@
+package de.accountio
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.features.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+
+        get("/") {
+            call.respond(mapOf("name" to "Accounting application", "version" to "0.0.1"))
+        }
+    }
+}
